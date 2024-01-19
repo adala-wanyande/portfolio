03a0afa99fbca18180281847b78c6265eb4d80fe
adala-wanyande <benardwanyande@gmail.com>
2024-01-19 17:38:31 +0300

    feat(js): add smooth scrolling to navigation

--- script.js
+++ script.js
@@ -60,3 +60,23 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('html').classList.add('fade-in');
 });
+
+document.addEventListener("DOMContentLoaded", makeLinksSmooth);
+
+function makeLinksSmooth() { 
+  const navLinks = document.querySelectorAll("nav a"); 
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
+  });
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const targetId = this.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+
+  if (targetElement) { 
+    targetElement.scrollIntoView({ behavior: "smooth", });
+  }
+}
